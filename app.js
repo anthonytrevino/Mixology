16b30d1fae9b0a3a682b4f8301be1eb03b12ae42
Kevin Parmer <jkparmer89@gmail.com>
2018-11-14 13:34:20 -0600

    Api working

--- app.js
+++ app.js
@@ -2,12 +2,57 @@ const express = require('express')
 const app = express()
 const mustacheExpress = require('mustache-express')
 const bodyParser = require('body-parser')
+const cocktailAPI = `https://www.thecocktaildb.com/api/json/v1/1/`
+var fetch = require('node-fetch')
+
 
 app.engine('mustache', mustacheExpress())
 app.set('views','./views')
 app.set('view engine','mustache')
 
 app.use(bodyParser.urlencoded({extended : false}))
+//pp.use('/jquery', express.static('jQuery.js'))
+
+
+
+app.post('/getCocktail', function(req, res){
+    let serchTerm = req.body.query
+    console.log(serchTerm)
+    fetch(cocktailAPI + "search.php?s=" + serchTerm).then(res => res.json()).then(json => {
+      //console.log(json)
+
+      json.drinks.map(function(drink){
+
+          drink.ingredients = [] 
+          
+
+          Object.keys(drink).map(function(key,value){
+            if(key.startsWith("strIngredient")) {
+              if(drink[key] != "") {
+                drink.ingredients.push(drink[key])
+              }
+            }
+          })
+
+          
+
+      })
+      console.log("HOLA")
+
+      //console.log(drink.ingredients)
+      res.render('result', {drinks: json.drinks})
+      
+    })
+   
+})
+
+// app.get('/result', function(req,res){
+//     //res.render('result', {json})
+// })
+
+app.get('/search', function(req, res){
+    res.render('search')
+})
 
 app.post('/login', function(req,res){
   res.redirect('login')
