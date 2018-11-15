026268a6c1c2f2b158b5938d97d5f172434530d7
Ryan Gawick <rgawick@gmail.com>
2018-11-15 13:24:53 -0600

    Login and Registration works

--- app.js
+++ app.js
@@ -11,22 +11,16 @@ app.use(session({
     saveUninitialized: false
   }))
 
-
 const pgp = require('pg-promise')()
-
 const cn = {
     host : 'ec2-54-83-38-174.compute-1.amazonaws.com',
     port : 5432,
     database : 'daa7lr4594n063',
     user: 'moqnblcbwdcfsg',
-    password:'',
+    password:'7d60c59b692b1f04f93c992a2226b602d1ea263c8e4d4ea2fad603749245f0b6',
     ssl:true
 }
-
 const db = pgp(cn)
-
-
-
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static('public'))
 app.engine('mustache',mustacheExpress())
@@ -54,13 +48,11 @@ app.get('/register', function(req,res){
         .catch(function(error){
             console.log(error)
         })
-
     })
 
     app.post('/login', function(req, res){
-         username = req.body.username
+        let username = req.body.username
         let password = req.body.password
-
         db.any('SELECT userid, username, password FROM user_account WHERE username =$1',[username])
         .then(function(result){
             let user = result.find(function(user){
@@ -69,7 +61,6 @@ app.get('/register', function(req,res){
                }else{
                    console.log('error')
                }
-
             })
             if(user != null){
                 if(user.username){
@@ -80,11 +71,9 @@ app.get('/register', function(req,res){
             }else{
                 res.redirect('index')
             }
-
         })
-
-
     })
+
 //post comments to db
 app.post('/enter_comment', function(req,res){
     let iddrink = req.body.iddrink
@@ -98,18 +87,9 @@ app.get('/drink_item', function(req, res){
 
 db.any('SELECT * FROM drink_comment WHERE iddrink = $1',[2])
 .then(function(result){
-
     res.render('drink_item', {drink_item: result})
 })
-
 })
-
-
-
-
-
-
-
 
 app.listen(3000,function(req,res){
     console.log("Server has started...")
